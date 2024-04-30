ec8f0af94af5b0c95afabeb57d5fc6ab7e364329
tisOrion <141913263+tisOrion@users.noreply.github.com>
2024-04-30 15:15:50 +0200

    First commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+
+TOPIC = "Herzfrequenz"
+BROKER_ADRESS = "localhost"
+PORT = 1883
+QOS = 1
+
+class mqttSend(object):
+    def __init__(self):
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client.connect(BROKER_ADRESS, PORT)
+        self.observer = None
+        
+        print("Connect to MQTT Broker: " + BROKER_ADRESS)
+        data = "{TEST_DATA}"
+        
+        client.publish(TOPIC, data, QOS)
+        client.loop()
+        
+    def __del_(self):
+        print("Nothing to delete. Existing.")
+        
+    def update(self, payload):
+        print("Payload: ", payload)
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client.connect(BROKER_ADRESS, PORT)
+        client.publish(TOPIC, payload, QOS)
+        client.loop()
