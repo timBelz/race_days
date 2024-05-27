ea11a97b87ec4f50818bfb4fb1af298856dd8cb7
timBelz <st172319@stud.uni-stuttgart.de>
2024-05-27 14:15:54 +0200

    integrated streaming

--- receiver_connection.py
+++ receiver_connection.py
@@ -7,23 +7,20 @@ from datetime import datetime, timezone
 broker_address = "192.168.178.202"
 port = 1883
 
-# InfluxDB Configuration
-url = 'http://localhost:8086/orgs/08762263b5ca87d3'
-token = '352VYvG1n5N17ayto3Fqc2ixcoDBdxhmPTt3WyHvn1eg1QJ6e1Ha0QkWEEAJ5JQL3VnbxrBjVLRpAdWXz6xCAA=='
-org = 'Race Days'
-bucket = 'sensor_viz'
-
 # Initialize InfluxDB client
-influxdb_client = InfluxDBClient(url=url, token=token, org=org)
+influxdb_client = InfluxDBClient(url="http://localhost:8086", 
+                                 token="352VYvG1n5N17ayto3Fqc2ixcoDBdxhmPTt3WyHvn1eg1QJ6e1Ha0QkWEEAJ5JQL3VnbxrBjVLRpAdWXz6xCAA==", 
+                                 org="Race Days"
+                                 )
 
 # Callback function for each sensor data type
 def on_message(client, userdata, message):
     # Create a new point and write it to InfluxDB
     with influxdb_client.write_api(write_options=SYNCHRONOUS) as write_api:
         p = Point(message.topic) \
-            .field("value", float(message.payload.decode('utf-8'))) \
+            .field("value", int(message.payload.decode('utf-8'))) \
             .time(datetime.now(timezone.utc), WritePrecision.MS)
-        write_api.write(bucket=bucket, record=p)
+        write_api.write(bucket='sensor_viz', record=p)
 
 # Initialize MQTT client and set callbacks
 client = mqtt.Client()
