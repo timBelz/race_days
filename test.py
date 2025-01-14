9b7b34156ea641a4251d028f65eed308de3beb59
timBelz <st172319@stud-uni.stuttgart.de>
2025-01-14 18:49:33 +0100

    a thing?

--- test.py
+++ test.py
@@ -1,39 +1,34 @@
+from flask import Flask, render_template, jsonify
+from influxdb_client import InfluxDBClient
 import yaml
-from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
-from datetime import datetime, timezone
-import time
-import random
 
-# Load credentials from YAML file
-with open("credentials.yaml", 'r') as stream:
+app = Flask(__name__)
+
+# Load credentials
+
+with open("credentials.yaml", "r") as stream:
     try:
         credentials = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
 
-# Initialize InfluxDB client
-influxdb_client = InfluxDBClient(url=credentials['influxdb']['url'], 
-                                 token=credentials['influxdb']['token'], 
-                                 org=credentials['influxdb']['org']
+influxdb_client = InfluxDBClient(url=credentials["influxdb"]["url"],
+                                 token=credentials["influxdb"]["token"],
+                                 org=credentials["influxdb"]["org"]
                                  )
 
-# Topics to generate dummy data for
-topics = ["herzfrequenz", "leistung", "geschwindigkeit"]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-while True: #infinite loop to generate data
-
-    for topic in topics:
-        # Generate a random integer between 25 and 100
-        value = random.randint(25, 100)
-
-        # Create a new point and write it to InfluxDB
-        with influxdb_client.write_api(write_options=SYNCHRONOUS) as write_api:
-            p = Point(topic) \
-                .field("value", value) \
-                .time(datetime.now(timezone.utc), WritePrecision.MS)
-            write_api.write(bucket='tests', record=p)
-
-        # Wait for 1 second before generating the next set of data
-        time.sleep(3)
-    
+@app.route('/data')
+def date():
+    query = 'from(bucket: "{}") |> range(start: -1m) |> filter(fn: (r) => r._measurement == "herzfrequenz")'.format(credentials['influxdb']['bucket'])
+    result = influxdb_client.query_api().query(org=credentials['influxdb']['org'], query=query)
+    points = []
+    for table in result:
+        for record in table.records:
+            points.append({"time": record.get_time(), "value": record.get_value()})
+            
+if __name__ == '__main__':
+    app.run(debug=True)
